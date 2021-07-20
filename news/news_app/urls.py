b28450747d9316227db557f08bccdc33a011278a
waiterorg <konjkav79@gmail.com>
2021-07-20 16:07:42 +0430

    create category list filter by category slug

--- news/news_app/urls.py
+++ news/news_app/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
-from .views import ArticleListView, ArticleDetail
+from .views import ArticleListView, ArticleDetail, CategoryList
 
 app_name = 'news'
 urlpatterns = [
     path('', ArticleListView.as_view(), name='articlelist'),
     path('page/<int:page>', ArticleListView.as_view(), name='articlelist'),
     path('news/<slug:slug>', ArticleDetail.as_view(), name='articledetail'),
+    path('category/<slug:slug>', CategoryList.as_view(), name='CategoryView'),
+    path('category/<slug:slug>/page/<int:page>', CategoryList.as_view(), name='CategoryView'),
+
 
 ]
--- news/news_app/views.py
+++ news/news_app/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import get_object_or_404, render
-from .models import Article
+from .models import Article, Category
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 # Create your views here.
@@ -21,3 +21,19 @@ class ArticleDetail(DetailView):
             article.hits.add(ip_address)
 
         return article
+
+class CategoryList(ListView):
+    paginate_by = 4
+    template_name = 'news_app/category_list.html'
+
+    def get_queryset(self):
+        global category
+        slug = self.kwargs.get('slug')
+        category = get_object_or_404(
+            Category.objects.get_active_category(), slug=slug)
+        return category.articles.get_published_article()
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['category'] = category
+        return context
