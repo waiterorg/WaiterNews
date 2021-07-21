0994229a266659cd356a7fe237e0ae83b108c529
waiterorg <konjkav79@gmail.com>
2021-07-21 17:17:27 +0430

    fix login page

--- news/news_app/urls.py
+++ news/news_app/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import ArticleListView, ArticleDetail, CategoryList, login
+from .views import ArticleListView, ArticleDetail, CategoryList
 
 app_name = 'news'
 urlpatterns = [
@@ -8,6 +8,5 @@ urlpatterns = [
     path('news/<slug:slug>', ArticleDetail.as_view(), name='articledetail'),
     path('category/<slug:slug>', CategoryList.as_view(), name='CategoryView'),
     path('category/<slug:slug>/page/<int:page>', CategoryList.as_view(), name='CategoryView'),
-    path('login/', login, name='login')
 
 ]
